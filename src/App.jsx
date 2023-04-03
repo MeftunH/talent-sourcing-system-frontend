d44ef3ca47bab9f6ac68bd1211e6c8dea046c716
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-04 02:33:29 +0300

    change logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import CandidateList from "./components/candidate/CandidateList.jsx";
 import AddCandidateModal from "./components/candidate/AddCandidateModal.jsx";
-
+import logo from '../public/TSS-logo.png';
 function App() {
     const [addCandidateModalIsOpen, setAddCandidateModalIsOpen] = useState(false);
     const openAddCandidateModalIsOpen = () => {
@@ -12,21 +12,22 @@ function App() {
     const closeAddCandidateModalIsOpen = () => {
         setAddCandidateModalIsOpen(false);
     };
+    return (
+        <div className="flex flex-col h-screen justify-center items-center">
+            <div className="mb-4 mt-40">
+                <img className="h-32 md:h-47 lg:h-60 w-100" src={logo} alt="Logo" />
 
-  return (
-      <div className="flex flex-col h-screen justify-center items-center">
-          <div className="mb-4">
-              <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={openAddCandidateModalIsOpen}
-              >
-                  Add Candidate
-              </button>
-              <AddCandidateModal isOpen={addCandidateModalIsOpen} onRequestClose={closeAddCandidateModalIsOpen} />
-          </div>
-          <CandidateList />
-      </div>
-  )
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={openAddCandidateModalIsOpen}
+                >
+                    Add Candidate
+                </button>
+                <AddCandidateModal isOpen={addCandidateModalIsOpen} onRequestClose={closeAddCandidateModalIsOpen} />
+            </div>
+            <CandidateList />
+        </div>
+    );
 }
 
 export default App
