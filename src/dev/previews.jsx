d4684e5e7f857ccc879ff0231d95564938407f68
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-03 14:54:22 +0300

    set previews

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import {PaletteTree} from './palette';
 import App from "../App.jsx";
+import CandidateList from "../components/candidate/CandidateList.jsx";
+import UpdateCandidateModal from "../components/candidate/UpdateCandidateModal.jsx";
+import ChangeStatusModal from "../components/candidate/actions/ChangeStatusModal.jsx";
+import AddCandidateModal from "../components/candidate/AddCandidateModal.jsx";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/App">
                 <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/CandidateList">
+                <CandidateList/>
+            </ComponentPreview>
+            <ComponentPreview path="/UpdateCandidateModal">
+                <UpdateCandidateModal/>
+            </ComponentPreview>
+            <ComponentPreview path="/ChangeStatusModal">
+                <ChangeStatusModal/>
+            </ComponentPreview>
+            <ComponentPreview path="/AddCandidateModal">
+                <AddCandidateModal/>
             </ComponentPreview>
         </Previews>
     );
